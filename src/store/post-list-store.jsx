b5055dd25a2f03c1e595512f6cdf527566d1b025
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2024-10-24 17:43:09 +0530

    new

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -57,19 +57,19 @@ const PostListProvider = ({ children }) => {
 const DEFAULT_POST_LIST = [
   {
     id: "1",
-    title: "Going to Mumbai",
-    body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.",
+    title: "Yeah we won the match",
+    body: "huraaahhhhh guys we won the match thanx to our captain and the GOAT virat and team",
     reactions: 2,
     userId: "user-9",
-    tags: ["vacation", "Mumbai", "Enjoying"],
+    tags: ["cricket", "Mumbai", "Enjoying"],
   },
   {
     id: "2",
-    title: "Paas ho bhai",
-    body: "4 saal ki masti k baad bhi ho gaye hain paas. Hard to believe.",
+    title: "Elections are comming",
+    body: "Excited to see kiski sarkaar banegi.",
     reactions: 15,
     userId: "user-12",
-    tags: ["Graduating", "Unbelievable"],
+    tags: ["Election", "India"],
   },
 ];
 
